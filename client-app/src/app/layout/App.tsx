bde8602333ff74fa668017133c80689506592338
kinderbueno360 <carlos.bueno@sapo.pt>
2021-08-09 10:56:42 +0200

    Add activity list and dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,33 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './styles.css';
 import  axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
     axios
-        .get('https://localhost:5001/api/activities')
+        .get<Activity[]>('https://localhost:5001/api/activities')
         .then(response=>{
           setActivities(response.data);
         })
   },[])
   
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x=>x.id === id));
+  }
+
+  function handleCancelActivity() {
+    setSelectedActivity(undefined);
+  }
+
   return (
-    <div className="App">
-      <Header as='h2' icon='users'  content="Reactitivies"/>
-      
-      <List>
-          {activities.map((activity: any)=>(
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-      </List>
-        
-    </div>
+    <>
+      <NavBar  />
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard 
+          activities={activities} 
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelActivity}
+        />
+      </Container>
+    </>
   );
 }
 
