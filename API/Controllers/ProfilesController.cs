8c5c069fd3f829034861dc26697ee00ef34d6e4f
kinderbueno360 <carlos.bueno@sapo.pt>
2021-10-06 23:44:20 +0200

    Profiles

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -10,7 +10,9 @@ namespace API.Controllers
     public class ProfilesController : BaseApiController
     {
         [HttpGet("{username}")]
-        public async Task<IActionResult> GetProfile(string username)
-            => HandleResult(await Mediator.Send(new Details.Query { UserName = username }));
+        public async Task<IActionResult> GetProfile(string username) => HandleResult(await Mediator.Send(new Details.Query { UserName = username }));
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(Edit.Command command) =>  HandleResult(await Mediator.Send(command));
     }
 }
